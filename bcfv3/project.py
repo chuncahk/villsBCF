64c81ad5049939b612ea3a115da84a18c9bf5104
Chuncahk <36185113+chuncahk@users.noreply.github.com>
2024-09-26 01:24:18 -0400

    Created object of bcf.version

--- bcfv3/project.py
+++ bcfv3/project.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import pathlib
 import os
 
-def create_bcfversion(projectPath = "",bcfVersion = ""):
+def create_bcf_version(projectPath = "",bcfVersion = ""):
     """
     Create bcf.version file
     """
@@ -16,3 +16,21 @@ def create_bcfversion(projectPath = "",bcfVersion = ""):
     ET.indent(root, space = "    ", level = 0)
     tree = ET.ElementTree(root)
     tree.write(f"{path}{os.sep}bcf.version", encoding='utf-8', xml_declaration=True)
+
+#def create_extensions_xml(projectPath = "",bcfVersion = ""):
+
+class BcfDotVersion():
+    __slots__ = ("Version")
+    def __init__(self, Version):
+        self.Version = Version
+    def __setattr__(self, attr, value):
+        value = self.check_attr_Version(value)
+        object.__setattr__(self, attr, value)
+    def check_attr_Version(self,val):
+        if (isinstance(val, int) or isinstance(val, float)):
+            return (f"{val:.1f}")
+        else:
+            raise AttributeError("Type of 'Version' must be Integer or Float")
+
+class ExtensionsDotXml:
+    __slots__ = ("Version")
